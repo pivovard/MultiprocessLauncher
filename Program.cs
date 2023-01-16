fab5e85bc1abe02832308e3fe406e4f808fd9b62
pivovard <pivovar.david@gmail.com>
2023-01-16 12:09:59 +0100

    Rework to set a screen position instead of moving a window by the shortcuts.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using WindowsInput;
@@ -7,6 +8,11 @@ namespace WinFormsApp1
 {
     internal static class Program
     {
+        private const int SW_SHOWMAXIMIZED = 3;
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         [DllImport("user32.dll", SetLastError = true)]
         private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
 
@@ -18,17 +24,17 @@ namespace WinFormsApp1
 
         static void Main(string[] args)
         {
-            //-p "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -a "--new-window www.google.com" -right 1
+            //-p "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -a "--new-window www.google.com" -screen 1
 
             if (args.Length > 0)
             {
                 if (args[0] == "-f" || args[0] == "--file")
                 {
-                    ParseFile(args[1]);
+                    ReadFile(args[1]);
                 }
                 else if (args[0] == "-p" || args[0] == "--proc")
                 {
-                    ParseCommand(args);
+                    ParseLine(args);
                 }
                 else
                 {
@@ -42,28 +48,28 @@ namespace WinFormsApp1
             }
         }
 
-        private static void ParseFile(string path)
+        private static void ReadFile(string path)
         {
             try
             {
                 IEnumerable<string> file = File.ReadLines(path);
                 foreach (var line in file)
                 {
-                    var args = ParseArguments(line);
+                    var args = SplitArguments(line);
 
                     if (args.Length > 0)
                     {
-                        ParseCommand(args);
+                        ParseLine(args);
                     }
                 }
             }
-            catch (Exception)
+            catch (FileNotFoundException)
             {
-                Console.WriteLine("Unable to read the file!");
+                Console.WriteLine($"Unable to read the config file {path}!");
             }
         }
 
-        static string[] ParseArguments(string commandLine)
+        static string[] SplitArguments(string commandLine)
         {
             char[] parmChars = commandLine.ToCharArray();
             bool inQuote = false;
@@ -77,11 +83,11 @@ namespace WinFormsApp1
             return (new string(parmChars)).Split('\n');
         }
 
-        private static void ParseCommand(string[] command)  
+        private static void ParseLine(string[] command)  
         {
             string proc = command[1];
             string args = "";
-            bool reset = false;
+            uint screen = 0;
 
             uint left = 0;
             uint up = 0;
@@ -94,9 +100,9 @@ namespace WinFormsApp1
                 {
                     args = command[i+1];
                 }
-                if (command[i] == "-r" || command[i] == "--reset")
+                if (command[i] == "-s" || command[i] == "--screen")
                 {
-                    reset = true;
+                    screen = uint.Parse(command[i + 1]);
                 }
 
                 if (command[i] == "-left")
@@ -117,15 +123,44 @@ namespace WinFormsApp1
                 }
             }
 
-            var process = Process.Start(proc, args);
-            Thread.Sleep(1000);
-
-            if (reset)
+            try
             {
-                SetWindowPosition(process, 0);
+                var process = Process.Start(proc, args);
+                Thread.Sleep(2000);
+
+                if (process.MainWindowHandle == IntPtr.Zero)
+                {
+                    //get process name from path
+                    string pname = proc.Split(new char[] { '/', '\\' }).Last().Split('.').First();
+                    process = Process.GetProcessesByName(pname).First(p => p.MainWindowHandle != IntPtr.Zero);
+                }
+
+                SetWindowPosition(process, screen);
+                MoveWindow(left, up, right, down);
+            }
+            catch (Win32Exception)
+            {
+                Console.WriteLine($"File not found at {proc}!");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"Getting window handler of process failed!");
+            }
+        }
+
+        private static void SetWindowPosition(Process process, uint screen)
+        {
+            process.WaitForInputIdle(5000);
+            if (System.Windows.Forms.Screen.AllScreens.Length > screen)
+            {
+                SetWindowPos(process.MainWindowHandle,
+                    IntPtr.Zero,
+                    Screen.AllScreens[screen].WorkingArea.Left,
+                    Screen.AllScreens[screen].WorkingArea.Top,
+                    0, 0, SWP_NOSIZE | SWP_NOZORDER);
             }
 
-            MoveWindow(left, up, right, down);
+            ShowWindow(process.MainWindowHandle, SW_SHOWMAXIMIZED);
         }
 
         private static void MoveWindow(uint left = 0, uint up = 0, uint right = 0, uint down = 0)
@@ -153,19 +188,6 @@ namespace WinFormsApp1
             sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.UP);
         }
 
-        private static void SetWindowPosition(Process process, int screen)
-        {
-            process.WaitForInputIdle(5000);
-            if (System.Windows.Forms.Screen.AllScreens.Length > screen)
-            {
-                SetWindowPos(process.MainWindowHandle,
-                    IntPtr.Zero,
-                    Screen.AllScreens[screen].WorkingArea.Left,
-                    Screen.AllScreens[screen].WorkingArea.Top,
-                    0, 0, SWP_NOSIZE | SWP_NOZORDER);
-            }
-        }
-
         private static List<IntPtr> GetHandlers(string name)
         {
             List<Process> existingProcesses = Process.GetProcessesByName(name).ToList();
@@ -181,14 +203,14 @@ namespace WinFormsApp1
         {
             Console.WriteLine("Process command: -p <path> -a <args> -left <int> -up <int> -right <int> -down <int> (-p is mandatory).\n");
             Console.WriteLine("\t\t-f --file <path> \t Path to the file with a list of processes to run.");
-            Console.WriteLine("\t\t-p --proc <precess> \t Process to run.");
+            Console.WriteLine("\t\t-p --proc <process> \t Process to run.");
             Console.WriteLine("\t\t-a --args <arguments> \t Run process with arguments.");
+            Console.WriteLine("\t\t-s --screen <screen> \t Move process to screen <screen>");
             Console.WriteLine();
             Console.WriteLine("\t\t-left  <int> \t\t Move left  by <int>.");
             Console.WriteLine("\t\t-up    <int> \t\t Move up    by <int>.");
             Console.WriteLine("\t\t-right <int> \t\t Move right by <int>.");
             Console.WriteLine("\t\t-down  <int> \t\t Move down  by <int>.");
-            Console.WriteLine("\t\t-r --reset \t\t Reset window position (doesn't work for multiple browser windows).");
         }
 
     }
